3a771a7cbb0a4446956b5f2bf4976a98ba3d4ca8
Maria Chapman <thunderstarry@gmail.com>
2012-12-28 17:07:10 +0000

    Added page to view all data in database
    Added top.html and bottom.html

--- hello.py
+++ hello.py
@@ -3,15 +3,32 @@ import os
 import database
 from database import Session, User
 
-class OnePage(object):
-	def index(self):
-		return "One page."
+top = open(os.path.join("static", "top.html"), "r").read()
+bottom = open(os.path.join("static", "bottom.html"), "r").read()
 
-	def thing(self):
-		return "Things"
+class AllUsers(object):
+	def index(self):
+		tableText = "<div class=\"container\">\n\
+			<table class=\"table\">\n\
+				<thead>\n\
+					<tr>\n\
+						<th>Name</th>\n\
+						<th>Username</th>\n\
+						<th>Password</th>\n\
+					</tr>\n\
+				</thead>\n\
+				<tbody>\n"
+		session = Session()
+		for user in session.query(User).order_by(User.id):
+			tableText += ("<tr>\n<td>" + user.fullname + "</td>\n" +
+			"<td>" + user.username + "</td>\n" +
+			"<td>" + user.password + "</td>\n</tr>\n")
+
+		tableText += "</tbody>\n</table>\n"
+		session.close()
+		return top + tableText + bottom
 
 	index.exposed = True
-	thing.exposed = True
 
 class Signup(object):
 	def index(self, fullname=None, username=None, password=None):
@@ -25,12 +42,13 @@ class Signup(object):
 	index.exposed = True
 
 class Root:
-	onepage = OnePage()
+	allusers = AllUsers()
 	signup = Signup()
 
 	def index(self):
 		html = open(os.path.join("static", "index.html"), "r").read()
-		return html
+		return top + html + bottom
+		
 	index.exposed = True
 
 cherrypy.quickstart(Root(), config="hello.config")
